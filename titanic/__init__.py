922a2f484b8d170071bd23bb6fd08296b63a7085
kimhs1876 <kimhs1876@naver.com>
2021-06-02 23:03:54 +0900

    titanic

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -2,25 +2,25 @@ from titanic.views.controller import Controller
 from titanic.templates.plot import Plot
 
 if __name__ == '__main__':
+    controller = Controller()
     while 1:
-        menu = input('0-exit\n'
-                     '1-data visualization\n'
-                     '2-machine learning\n'
-                     '3-machine release')
-        if menu =='0':
+        menu = input('0-exit 1-data visualization\n'
+                     ' 2-modeling\n'
+                     ' 3-machine learning\n'
+                     ' 4-machine release ')
+        if menu == '0':
             break
         elif menu == '1':
-            plot = Plot('train.csv') #?
-            plot.draw_survived_dead()
-            plot.draw_Pclass()
-            plot.draw_sex()
+            plot = Plot('train.csv')
+            # plot.draw_survived_dead()
+            # plot.draw_pclass()
+            # plot.draw_sex()
             plot.draw_embarked()
         elif menu == '2':
-            controller = Controller()
-            controller.modeling('train.csv', 'test.csv') #?
+            df = controller.modeling('train.csv','test.csv')
         elif menu == '3':
-            pass
+            controller.learning(df)
         elif menu == '4':
-            pass
+            controller.submit('train.csv','test.csv')
         else:
             continue
--- titanic/models/service.py
+++ titanic/models/service.py
@@ -1,5 +1,10 @@
 from titanic.models.dataset import Dataset
 import pandas as pd
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
 
 class Service(object):
 
@@ -13,14 +18,94 @@ class Service(object):
 
     @staticmethod
     def create_train(this) -> object:
-        return this.train.drop('Survived', axis =1) # ??
+        return this.train.drop('Survived', axis = 1)
 
     @staticmethod
-    def create_lable(this):
-        return this.train['Survived'] # ??
+    def create_label(this) -> object:
+        return this.train['Survived']
 
     @staticmethod
-    def drop_feature(this, feature) -> object:
-        this.train = this.train.drop([feature], axis =1) # ??
-        this.test = this.test.drop([feature], axis=1) # ??
-        return this # ??
+    def drop_feature(this, *feature) -> object:
+        for i in feature:
+            this.train = this.train.drop([i], axis=1)
+            this.test = this.test.drop([i], axis=1)
+        return this
+
+    @staticmethod
+    def embarked_nominal(this) -> object:
+        this.train = this.train.fillna({'Embarked':'S'})
+        this.test = this.test.fillna({'Embarked':'S'})
+        this.train['Embarked'] =  this.train['Embarked'].map({'S':1,'C':2,'Q':3})
+        this.test['Embarked'] =  this.test['Embarked'].map({'S':1,'C':2,'Q':3})
+        return this
+
+    @staticmethod
+    def fare_ordinal(this) -> object:
+        this.test['Fare'] = this.test['Fare'].fillna(1)
+        this.train['FareBand'] = pd.qcut(this.train['Fare'], 4)
+        # print(f'qcut 으로 bins 값 설정 {this.train["FareBand"].head()}')
+        bins = [-1, 8, 15, 31, np.inf]
+        this.train = this.train.drop(['FareBand'], axis=1)
+        for these in [this.train, this.test]:
+            these['FareBand'] = pd.cut(these['Fare'], bins=bins, labels=[1, 2, 3, 4])
+        return this
+
+    @staticmethod
+    def title_norminal(this) -> object:
+        combine = [this.train, this.test]
+        for dataset in combine:
+            dataset['Title'] = dataset.Name.str.extract('([A-Za-z]+)\.', expand=False)
+        for dataset in combine:
+            dataset['Title'] = dataset['Title'].replace(['Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Jonkheer', 'Dona'], 'Rare')
+            dataset['Title'] = dataset['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+            dataset['Title'] = dataset['Title'].replace('Mlle', 'Mr')
+            dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+            dataset['Title'] = dataset['Title'].replace('Mme', 'Rare')
+            title_mapping = {'Mr': 1, 'Miss': 2, 'Mrs': 3, 'Master': 4, 'Royal': 5, 'Rare': 6}
+            dataset['Title'] = dataset['Title'].fillna(0)
+            dataset['Title'] = dataset['Title'].map(title_mapping)
+        return this
+
+    @staticmethod
+    def gender_norminal(this) -> object:
+        gender_mapping = {'male': 0, 'female': 1}
+        for these in [this.train, this.test]:
+            these['Gender'] = these['Sex'].map(gender_mapping)
+        return this
+
+    @staticmethod
+    def age_ordinal(this) -> object:
+        train = this.train
+        test = this.test
+        train['Age'] = train['Age'].fillna(-0.5)
+        test['Age'] = test['Age'].fillna(-0.5)
+        bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf]
+        labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+        age_title_mapping = {'Unknown':0 , 'Baby': 1, 'Child': 2, 'Teenager' : 3, 'Student': 4, 'Young Adult': 5,
+                             'Adult':6,  'Senior': 7}
+        for i in train, test:
+            i['AgeGroup'] = pd.cut(i['Age'], bins=bins, labels=labels) # {[labels]:[bins]}
+            i['AgeGroup'] = i['AgeGroup'].map(age_title_mapping)
+
+        return this
+
+    @staticmethod
+    def create_k_fold() -> object:
+        return KFold(n_splits=10, shuffle=True, random_state=0)
+
+    @staticmethod
+    def accuracy_by_svm(this):
+        print(f'10. Train 의 type \n {type(this.train)} ')
+        print(f'11. Train 의 column \n {this.train.columns} ')
+        print(f'12. Train 의 상위 1개 행\n {this.train.head()} ')
+        print(f'13. Train 의 null 의 갯수\n {this.train.isnull().sum()}개')
+        print(f'14. label \n {type(this.label)}')
+        score = cross_val_score(RandomForestClassifier(),
+                                this.train,
+                                this.label,
+                                cv=KFold(n_splits=10,
+                                         shuffle=True,
+                                         random_state=0),
+                                n_jobs=1,
+                                scoring='accuracy')
+        return round(np.mean(score) * 100, 2)
